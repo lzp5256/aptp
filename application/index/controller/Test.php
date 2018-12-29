3e2807e2500c9ab3ac58a6ffdb62e9423c405ae2
李志鹏 <244965712@qq.com>
2018-12-29 19:18:03 +0800

    1.初始化项目代码

---
+++ application/index/controller/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lizhipeng
+ * Date: 2018/12/29
+ * Time: 7:16 PM
+ */
+namespace app\index\controller;
+
+class Test
+{
+    public function test1()
+    {
+        return 'hello test1';
+    }
+}
