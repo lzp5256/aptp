262783ea694ec43ea1ca14d3f592315d5fa52cbc
李志鹏 <244965712@qq.com>
2019-01-03 15:09:57 +0800

    修改路由

--- application/route.php
+++ application/route.php
@@ -14,7 +14,7 @@ Route::group('test',function (){
 // 微信相关路由
 Route::group('wechat',function(){
     // 获取微信
-    Route::post('GetToken','wechat/wechat/getWechatToken');
+    Route::post('GetToken','wechat/token/getWechatToken');
 });
 
 // 七牛相关路由
