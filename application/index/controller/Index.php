5f7773b95e91d7a5bc8f718b7e904dfda174a6a0
李志鹏 <244965712@qq.com>
2019-02-12 15:28:52 +0800

    新增首页列表方法

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,15 +2,36 @@
 namespace app\index\controller;
 
 use app\base\controller\Base;
+use app\demand\model\Demand;
+
 
 class Index extends Base
 {
     public function __construct()
     {
-        $check = parent::__construct();
-        if($check != true){
-            return json($check);
+        $result = parent::__construct();
+        if($result['errCode'] != '200'){
+            echo $result['errMsg'];die;
         }
+    }
+
+    public function getReList()
+    {
+        $Result = [
+            'errCode' => '200',
+            'errMsg'  => 'success',
+            'data'    => [],
+        ];
+        $params = request()->param();
+        $model = new Demand();
+        $data = $model->selectDemand(true,0,10);
+        if(count($data)<=0){
+            $Result['errCode'] = 'L10029';
+            $Result['errMsg'] = '抱歉，暂无数据！';
+        }else{
+            $Result['data'] = collection($data)->toArray();
+        }
+        return json($Result);
     }
 
 
