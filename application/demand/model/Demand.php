3ca95c7e4a63e9ca6530d2356dda9cbd6db13a2e
李志鹏 <244965712@qq.com>
2019-02-12 15:32:33 +0800

    新增查询方法

--- application/demand/model/Demand.php
+++ application/demand/model/Demand.php
@@ -24,4 +24,20 @@ class Demand extends Model
         $this->data($data);
         return $this->save();
     }
+
+    /**
+     * 查找用户信息
+     *
+     * @param array $where 查询条件
+     * @param string $field 查询字段 默认为全部
+     * @param string $order 排序方式 默认id倒序
+     * @param int $offset 查询页数
+     * @param int $num 查询条
+     *
+     * @return array
+     */
+    public function selectDemand($where,$offset,$num,$field='*',$order='id desc')
+    {
+        return $this->where($where)->field($field)->order($order)->limit("$offset,$num")->select();
+    }
 }
