8bfa652881c3946069c928abbd91993c3767312e
李志鹏 <244965712@qq.com>
2019-08-03 17:08:36 +0800

    update article

--- application/controllers/controller/Article.php
+++ application/controllers/controller/Article.php
@@ -1,10 +1,11 @@
 <?php
 namespace app\controllers\controller;
 
+use app\base\controller\Base;
 use app\event\ArticleCheck;
 use app\event\ArticleHandles;
 
-class Article
+class Article extends Base
 {
     public function toInfo()
     {
@@ -26,5 +27,22 @@ class Article
         }
         $res['data'] = $handles_res['data'];
         return json($res);
+    }
+
+    public function toList()
+    {
+        $param = request()->post();
+        $check_event   = new ArticleCheck();
+        $handles_event = new ArticleHandles();
+
+        if(($check_res = $check_event->checkToListParams($param)) && $check_res['errCode'] != '200'){
+            return json($check_res);
+        }
+
+        if(($handles_res = $handles_event->setData($check_res['data'])->handleToListRes()) && $handles_res['errCode'] != '200'){
+            return json($handles_res);
+        }
+
+        return json($this->setReturnMsg('200',$handles_res['data']));
     }
 }
