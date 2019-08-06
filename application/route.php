47f122b0025573cc08ebf1dbbc45e15cfe773166
李志鹏 <244965712@qq.com>
2019-08-06 18:06:09 +0800

    add comment func && route

--- application/route.php
+++ application/route.php
@@ -113,6 +113,7 @@ Route::group('article',function (){
     Route::post('info','controllers/article/toInfo'); // 获取文章详情
     Route::post('recommend','controllers/article/toRecommend'); // 获取推荐文章
     Route::post('browse','controllers/article/toBrowse');   // 浏览
+    Route::post('comment','controllers/article/toComment'); // 评论
 });
 
 /**************************************  ask route **************************************/
